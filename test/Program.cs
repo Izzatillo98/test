a990e53eec19ca27adc5ca1b7b0966ec26e265d3
Izzatillo Abdusamatov <abdusamatovtillo8@gmail.com>
2024-08-23 22:31:47 +0500

    Add project files.

---
+++ test/Program.cs
@@ -0,0 +1,33 @@
+﻿using test.model;
+
+namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student;
+            ShowStudentInfo(out student);
+            Console.WriteLine(student.Surname);
+        }
+        //params
+        static void Add(params int[]b) 
+        {
+            int sum = 0;
+            for (int i = 0; i < b.Length; i++)
+            { 
+                sum += b[i];
+            }
+            Console.WriteLine(sum);
+        }
+        static void ShowStudentInfo(out Student student)
+        {
+            student = new Student("Tillo");
+            Console.WriteLine(student.Name);
+            Console.WriteLine(student.Surname);
+            student.Surname = "Abdusamatov";
+            
+        }
+    }
+
+}
---
+++ test/model/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test.model
+{
+    internal struct  Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public Student(string name )
+        {
+            Name = name;   
+        }
+    }
+}
